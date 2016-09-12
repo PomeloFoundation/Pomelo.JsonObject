c5ceff17c7b7b3f5b6efd783d63916aba4e47e03
Caleb Lloyd <caleblloyd@gmail.com>
2016-09-12 00:07:19 -0400

    initial commit

---
+++ src/Pomelo.JsonObject/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Pomelo Foundation. All rights reserved.
+// Licensed under the MIT. See LICENSE in the project root for license information.
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("Pomelo.JsonObject")]
+[assembly: AssemblyDescription("JSON Object optomized for MySql 5.7+ and Entity Framework Core")]
---
+++ test/Pomelo.JsonObject.Tests/System/JsonObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pomelo.JsonObject.Tests.System
+{
+
+    [TestClass]
+    public class JsonObjectTest
+    {
+
+        protected T Cast <T>(string serialized)
+        {
+            var dataParam = Expression.Parameter(typeof(string), "data");
+            var body = Expression.Block(Expression.Convert(dataParam, typeof(T)));
+            var run = Expression.Lambda(body, dataParam).Compile();
+            return (T) run.DynamicInvoke(serialized);
+        }
+
+        [TestMethod]
+        public void TestJsonObject()
+        {
+            const string jsonStr = @"{""a"":""b""}";
+            var jsonObj = Cast<JsonObject<Dictionary<string, string>>>(jsonStr);
+            Assert.IsTrue(jsonObj.Object.ContainsKey("a"));
+            Assert.AreEqual("b", jsonObj.Object["a"]);
+            Assert.AreEqual(jsonStr, jsonObj.Json);
+        }
+    }
+
+}
